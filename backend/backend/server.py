17fb5244926cbd67b1120737931ba7b3191d97e6
Victors1967 <victorsmirnov@me.com>
2021-02-16 12:21:46 +0200

    111

--- backend/backend/server.py
+++ backend/backend/server.py
@@ -14,3 +14,7 @@ app = WikiApp('wiki-app')
 env = os.environ.get('APP_ENV', 'dev')
 print(f'Starting application in {env} mode')
 app.config.from_object(f'backend.{env}_settings')
+
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1><p>From backend</p>'
